fbaeb532739ebc972bd4419a0cbb57c2b637a3af
saisripooja <psspooja1722@gmail.com>
2025-04-30 11:38:50 +0530

    Delete RTL/INCA_libs directory

--- RTL/INCA_libs/irun.lnx8664.15.20.nc/files.ts
+++
@@ -1,4 +0,0 @@
-1742204715 /home/user/pooja/adder completed/rtl/cds.lib
-1742204715 /home/user/pooja/adder completed/rtl/hdl.var
-1742204635 /home/user/pooja/adder completed/rtl/fulladd_tb.v
-1742204633 /home/user/pooja/adder completed/rtl/fulladd.v
